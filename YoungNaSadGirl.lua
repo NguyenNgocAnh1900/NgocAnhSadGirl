16f41fc4612b83360b96a1f400d4077777026be8
NguyenNgocAnh1900 <139627893+NguyenNgocAnh1900@users.noreply.github.com>
2023-08-28 05:02:18 -0700

    YoungNaSadGirl.lua

---
+++ YoungNaSadGirl.lua
@@ -0,0 +1,21 @@
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Cyan>To Everyone Who Want Use This Hub!<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Orange>If You Wanna Script Update Please:!<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Purple>Donate to: MOMO / NguyenNgocAnh / 0937277946<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Red>Donate Pls To Update Hub<Color=/>"):Display()
+end)
+spawn(function()
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/NguyenNgocAnh1900/DoungGoSitinh/main/NASuyTinh.lua"
+  ))()
+end)
