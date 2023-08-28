8a66cb63aeaad2a43fc7e7191125d02faa2fcae7
NguyenNgocAnh1900 <139627893+NguyenNgocAnh1900@users.noreply.github.com>
2023-08-28 05:25:35 -0700

    NaThatSad.lua

--- NaThatSad.lua
+++ NaThatSad.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenNgocAnh1900/NgocAnhSadGirl/main/YoungNaSadGirl.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenNgocAnh1900/NgocAnhSadGirl/main/NaBuonQua.lua"))()
